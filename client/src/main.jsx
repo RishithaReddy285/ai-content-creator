09d145634a2c7fc81ee1bf9ca8ae05fbdc34e6d2
rishitha-tech-28 <frootireddy986@gmail.com>
2026-05-22 09:56:37 +0530

    Fix Vercel frontend deployment

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,8 +4,13 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
+import axios from 'axios'
 import App from './App'
 import './styles.css'
+
+if (import.meta.env.VITE_API_URL) {
+  axios.defaults.baseURL = import.meta.env.VITE_API_URL
+}
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
